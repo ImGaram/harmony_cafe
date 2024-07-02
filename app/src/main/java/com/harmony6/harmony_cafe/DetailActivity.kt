b8382b0108ba2e476d96242db6f18f702336d374
taeyoung <rlaxodud911@gmail.com>
2024-07-02 19:56:52 +0900

    추가: 화면 상단에 툴바 동작 추가

--- app/src/main/java/com/harmony6/harmony_cafe/DetailActivity.kt
+++ app/src/main/java/com/harmony6/harmony_cafe/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.harmony6.harmony_cafe
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +18,18 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.detail_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
