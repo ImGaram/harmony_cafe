6ff20c3538ccabfcb980259035869df96a507a27
kkevi <bora_kim980@naver.com>
2024-07-03 15:25:29 +0900

    추가 : 마이페이지 인텐트 추가 및 로그인에서 UserData 받아오기

--- app/src/main/java/com/harmony6/harmony_cafe/HomeActivity.kt
+++ app/src/main/java/com/harmony6/harmony_cafe/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.harmony6.harmony_cafe
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +20,22 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
-        val feedProfileImage = findViewById<ImageView>(R.id.home_feed_title_profile1)
+        val detailImage1 = findViewById<ImageView>(R.id.home_imageView1)
+        val detailImage2 = findViewById<ImageView>(R.id.home_imageView2)
+        val detailImage3 = findViewById<ImageView>(R.id.home_imageView3)
+        val detailImage4 = findViewById<ImageView>(R.id.home_imageView4)
+        val detailImage5 = findViewById<ImageView>(R.id.home_imageView5)
+
+        val nameTextView = findViewById<TextView>(R.id.home_welcome_message)
+        val myPageImageButton = findViewById<ImageView>(R.id.imageButton)
+
+        val userData = intent.getParcelableExtra<User>("user")
+        nameTextView.text = getString(R.string.home_welcome_message, userData?.name)
+
+        myPageImageButton.setOnClickListener{
+            val myPageIntent = Intent(this, MyPageActivity::class.java)
+            myPageIntent.putExtra("user", userData)
+            startActivity(myPageIntent)
+        }
     }
 }
