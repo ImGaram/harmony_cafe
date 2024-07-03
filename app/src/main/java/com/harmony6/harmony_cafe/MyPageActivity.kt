02f363b8f5c6b5e517d4de49dc3df6441f0a108e
ImGaram <gim78014@gmail.com>
2024-07-03 18:07:38 +0900

    생성: 메뉴 목록 받아와 랜덤한 메뉴가 보여지도록 기능 구현

--- app/src/main/java/com/harmony6/harmony_cafe/MyPageActivity.kt
+++ app/src/main/java/com/harmony6/harmony_cafe/MyPageActivity.kt
@@ -1,14 +1,13 @@
 package com.harmony6.harmony_cafe
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
+import com.harmony6.harmony_cafe.data.MenuObject
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,5 +50,33 @@ class MyPageActivity : AppCompatActivity() {
 
         val backImage = findViewById<ImageView>(R.id.my_page_image_back)
         backImage.setOnClickListener { finish() }
+
+        val menuList = MenuObject.menuList
+        val numList = (0..4).toList()
+        val randomMenu = numList.shuffled().take(3)
+
+        val menuName1 = findViewById<TextView>(R.id.my_page_text_menu_name1)
+        val menuName2 = findViewById<TextView>(R.id.my_page_text_menu_name2)
+        val menuName3 = findViewById<TextView>(R.id.my_page_text_menu_name3)
+
+        menuName1.text = menuList[randomMenu[0]].name
+        menuName2.text = menuList[randomMenu[1]].name
+        menuName3.text = menuList[randomMenu[2]].name
+
+        val menuImage1 = findViewById<ImageView>(R.id.my_page_image_menu1)
+        val menuImage2 = findViewById<ImageView>(R.id.my_page_image_menu2)
+        val menuImage3 = findViewById<ImageView>(R.id.my_page_image_menu3)
+
+        menuImage1.setImageResource(menuList[randomMenu[0]].img)
+        menuImage2.setImageResource(menuList[randomMenu[1]].img)
+        menuImage3.setImageResource(menuList[randomMenu[2]].img)
+
+        val menuDescription1 = findViewById<TextView>(R.id.my_page_text_menu_description1)
+        val menuDescription2 = findViewById<TextView>(R.id.my_page_text_menu_description2)
+        val menuDescription3 = findViewById<TextView>(R.id.my_page_text_menu_description3)
+
+        menuDescription1.text = menuList[randomMenu[0]].desc
+        menuDescription2.text = menuList[randomMenu[1]].desc
+        menuDescription3.text = menuList[randomMenu[2]].desc
     }
 }
