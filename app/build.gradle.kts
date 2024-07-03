d6f3d4f6f7e2073b4378c8f124ca5af8e660e94b
wjsghk7664 <wjsghk7664@naver.com>
2024-07-03 15:29:44 +0900

    수정 minsdk변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.harmony6.harmony_cafe"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
