4b65a1b0bf55fd0a8d07b73b12db845205b53d80
ImGaram <gim78014@gmail.com>
2024-07-03 16:25:27 +0900

    추가: 유저 데이터 받아와서 텍스트뷰에 적용

--- app/src/main/java/com/harmony6/harmony_cafe/MyPageActivity.kt
+++ app/src/main/java/com/harmony6/harmony_cafe/MyPageActivity.kt
@@ -1,11 +1,14 @@
 package com.harmony6.harmony_cafe
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,34 @@ class MyPageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val userData = intent.getParcelableExtra<User>("user")
+        val userNameText = findViewById<TextView>(R.id.my_page_text_user_name)
+        val userIdText = findViewById<TextView>(R.id.my_page_text_user_id)
+        val userIntroduceText = findViewById<TextView>(R.id.my_page_text_user_introduce)
+
+        userNameText.text = userData?.name
+        userIdText.text = userData?.id
+        userIntroduceText.text = userData?.introduce
+
+        var isMoreClicked = false
+        userIntroduceText.setOnClickListener {
+            if (userIntroduceText.lineCount == 1 && !isMoreClicked) {
+                userIntroduceText.maxLines = 5
+                isMoreClicked = true
+            } else {
+                userIntroduceText.maxLines = 1
+                isMoreClicked = false
+            }
+        }
+
+        val userMenu1 = findViewById<TextView>(R.id.my_page_text_user_menu1)
+        val userMenu2 = findViewById<TextView>(R.id.my_page_text_user_menu2)
+        val userMenu3 = findViewById<TextView>(R.id.my_page_text_user_menu3)
+
+        userMenu1.text = getString(R.string.my_page_user_menu1, userData?.name)
+        userMenu2.text = getString(R.string.my_page_user_menu2, userData?.name)
+        userMenu3.text = getString(R.string.my_page_user_menu3, userData?.name)
 
         val backImage = findViewById<ImageView>(R.id.my_page_image_back)
         backImage.setOnClickListener { finish() }
